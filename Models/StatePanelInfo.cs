90c542f69c9e3f1cdb2f045fef4066dd4258841f
Baocheng Liu <1951073872@qq.com>
2024-06-15 17:09:45 +0800

    Home界面更新

---
+++ Models/StatePanelInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily.WPF.Models
+{
+    /// <summary>
+    /// 首页统计面板
+    /// </summary>
+    public class StatePanelInfo
+    {
+        /// <summary>
+        /// 统计项的图标
+        /// </summary>
+        public string Icon { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 统计项的名称
+        /// </summary>
+        public string ItemName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 统计结果
+        /// </summary>
+        public string Result { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 统计颜色
+        /// </summary>
+        public string Color { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 点击 跳转界面名称
+        /// </summary>
+        public string ViewName { get; set; } = string.Empty;
+
+
+        public StatePanelInfo(string icon,string itemName,string result,string color,string viewName)
+        {
+            Icon = icon;
+            ItemName = itemName;
+            Result = result;
+            Color = color;
+            ViewName = viewName;
+        }
+
+        public StatePanelInfo()
+        {
+            
+        }
+
+    }
+}
--- ViewModels/UCs/HomeUCViewModel.cs
+++ ViewModels/UCs/HomeUCViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Daily.WPF.Models;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,30 @@ namespace Daily.WPF.ViewModels.UCs
 {
     public class HomeUCViewModel : BindableBase
     {
+        public HomeUCViewModel()
+        {
+            _StatePanels = new List<StatePanelInfo>() 
+            { 
+                new StatePanelInfo("ClockFast","汇总","9","#FF0CA0FF","WaitUC"),
+                new StatePanelInfo("ClockCheckOutline","已完成","9","#FF1ECA3A","WaitUC"),
+                new StatePanelInfo("ChartLineVariant","完成比例","90%","#FF02C6DC",""),
+                new StatePanelInfo("PlaylistStar","备忘录","20","#FFFFA000","MemoUC"),
+            };
+
+        }
+
+        /// <summary>
+        /// 统计面板数据
+        /// </summary>
+        private List<StatePanelInfo> _StatePanels;
+
+		public List<StatePanelInfo> StatePanels
+		{
+			get { return _StatePanels; }
+			set => SetProperty(ref _StatePanels, value);
+		}
 
 
-    }
+
+	}
 }
