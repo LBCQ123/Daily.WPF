ad7573dfd9735d3b56e7ac14d1eaa03969967746
Baocheng Liu <1951073872@qq.com>
2024-06-17 07:16:05 +0800

    家改

---
+++ DTO/WaitInfoDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily.WPF.DTO
+{
+    /// <summary>
+    /// 待办事项DTO
+    /// </summary>
+    public class WaitInfoDTO
+    {
+        /// <summary>
+        /// 待办事项ID
+        /// </summary>
+        public int WaitId { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+
+        public WaitInfoDTO()
+        {
+            
+        }
+
+        public WaitInfoDTO(int waitId,string title,string content,int status)
+        {
+            WaitId = waitId;
+            Title = title;
+            Content = content;
+            Status = status;
+        }
+
+    }
+}
--- ViewModels/UCs/HomeUCViewModel.cs
+++ ViewModels/UCs/HomeUCViewModel.cs
@@ -1,10 +1,7 @@
-﻿using Daily.WPF.Models;
+﻿using Daily.WPF.DTO;
+using Daily.WPF.Models;
 using Prism.Mvvm;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Daily.WPF.ViewModels.UCs
 {
@@ -12,12 +9,18 @@ namespace Daily.WPF.ViewModels.UCs
     {
         public HomeUCViewModel()
         {
-            _StatePanels = new List<StatePanelInfo>() 
-            { 
+            _StatePanels = new List<StatePanelInfo>()
+            {
                 new StatePanelInfo("ClockFast","汇总","9","#FF0CA0FF","WaitUC"),
                 new StatePanelInfo("ClockCheckOutline","已完成","9","#FF1ECA3A","WaitUC"),
                 new StatePanelInfo("ChartLineVariant","完成比例","90%","#FF02C6DC",""),
                 new StatePanelInfo("PlaylistStar","备忘录","20","#FFFFA000","MemoUC"),
+            };
+
+            _WaitInfos = new List<WaitInfoDTO>()
+            {
+                new WaitInfoDTO(0,"会议一","会议内容，讨论并确立项目需求",0),
+                new WaitInfoDTO(1,"会议二","所有项目干系人都要参与，会议内容，讨论并确立项目目标",1)
             };
 
         }
@@ -27,13 +30,26 @@ namespace Daily.WPF.ViewModels.UCs
         /// </summary>
         private List<StatePanelInfo> _StatePanels;
 
-		public List<StatePanelInfo> StatePanels
-		{
-			get { return _StatePanels; }
-			set => SetProperty(ref _StatePanels, value);
-		}
+        public List<StatePanelInfo> StatePanels
+        {
+            get { return _StatePanels; }
+            set => SetProperty(ref _StatePanels, value);
+        }
+
+
+        /// <summary>
+        /// 待办事项列表
+        /// </summary>
+        private List<WaitInfoDTO> _WaitInfos;
+
+        public List<WaitInfoDTO> WaitInfos
+        {
+            get { return _WaitInfos; }
+            set => SetProperty(ref _WaitInfos, value);
+        }
 
 
 
-	}
+
+    }
 }
